2574bc33702a95884d16ac60cbdc5ca885a498e1
SM Melamed <5684686+SalmonMoses@users.noreply.github.com>
2021-06-15 21:28:18 +0300

    Update consts.js

--- src/components/consts.js
+++ src/components/consts.js
@@ -1,2 +1,2 @@
-export const path = process.env.REACT_APP_BACKEND_URL;
-// export const path = "https://31.43.111.158:8088/";
+// export const path = process.env.REACT_APP_BACKEND_URL;
+export const path = "https://31.43.111.158:8088/";
